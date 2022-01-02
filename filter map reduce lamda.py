4c245abc6fe099dfcd324862b7408308c3773df5
Sumeet Tiwari <tiwari.sumeet@gmail.com>
2022-01-02 15:47:27 +0530

    filter program update

--- filter map reduce lamda.py
+++ filter map reduce lamda.py
@@ -1,8 +1,17 @@
 
 
+#filter using labda
+#nums = [3,2,4,6,8,9,5]
+
+#evens = list(filter(lambda n : n%2==0,nums))
+
+#print (evens)
+
+# to reduce out of chunk we use map
 
 nums = [3,2,4,6,8,9,5]
 
 evens = list(filter(lambda n : n%2==0,nums))
+doubles = list(map(lambda n : n*2,evens))
 
-print (evens)
+print (doubles)

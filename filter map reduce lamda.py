b88dc9f2bb67d6d0367d9fc88643fe88af7d4797
Sumeet Tiwari <tiwari.sumeet@gmail.com>
2022-01-02 15:54:34 +0530

    filter program update

--- filter map reduce lamda.py
+++ filter map reduce lamda.py
@@ -9,9 +9,25 @@
 
 # to reduce out of chunk we use map
 
+#nums = [3,2,4,6,8,9,5]
+
+#evens = list(filter(lambda n : n%2==0,nums))
+#doubles = list(map(lambda n : n*2,evens))
+
+#print (doubles)
+
+#reduce the values, instead having multiple value we will have only one value, lets add all values using reduce
+from functools import reduce
+def add_all(a,b):
+    return a+b
+
 nums = [3,2,4,6,8,9,5]
 
 evens = list(filter(lambda n : n%2==0,nums))
 doubles = list(map(lambda n : n*2,evens))
+print(doubles)
 
-print (doubles)
+sum = reduce(lambda a,b : a+b, doubles)
+
+
+print (sum)
